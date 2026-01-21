ca5e171504b31d00741b8ce01f242f44a3e6a2b6
pjames <pjames@gmail.com>
2026-01-21 16:29:39 +0700

    Implement script aggregator-ioc.rb

--- 99-deployments/manifests/logstash-aggregator/scripts/aggregator-ioc.rb
+++ 99-deployments/manifests/logstash-aggregator/scripts/aggregator-ioc.rb
@@ -7,13 +7,41 @@ require 'securerandom'
 require 'ipaddr'
 require 'csv'
 require 'nokogiri'
+require 'redis'
+
+def connect_redis()
+    redisHost = ENV["REDIS_HOST"]
+    begin
+        r = Redis.new(
+          :host => redisHost,
+          :port => 6379,
+        )
+
+        client_ping = r.ping
+        if (client_ping)
+          puts("INFO : Connected to Redis [#{redisHost}]")
+        else
+          raise 'Ping failed!!!'
+        end
+    rescue => e
+        puts("ERROR: #{e}")
+        exit 100
+    end
+
+    return r
+end
 
 def register(params)
     $stdout.sync = true
+    @redisObj = connect_redis()
 end
 
 def populate_ts_aggregate(event)
+end
 
+def set_cache_key_value(cache, aggrKey, hour, value)
+  cache.set(aggrKey, value)
+  cache.expire(aggrKey, 3600 * hour)
 end
 
 def filter(event)
@@ -22,6 +50,51 @@ def filter(event)
     event.set('cust_pod_name_aggregator', ENV["POD_NAME"])
 
     line = event.get('message')
+
+    dataSet = event.get('[event][dataset]')
+    srcIp = event.get('[source][ip]')
+    dstIp = event.get('[destination][ip]')
+    dnsQuestionDomain = event.get('[dns][question][name]')
+    httpDomain = event.get('[url][domain]')
+    fileHashSha256 = event.get('[file][hash][sha256]')
+    fileHashMD5 = event.get('[file][hash][md5]')
+    fileHashSha1 = event.get('[file][hash][sha1]')
+
+    if srcIp && !srcIp.empty?
+        iocType = 'SourceIP'
+        iocCacheKeySeen = "IOC_SEEN!!#{iocType}!!#{dataSet}!!#{srcIp}"
+        set_cache_key_value(@redisObj, iocCacheKeySeen, 30, Time.now.to_i)
+    end
+
+    if dstIp && !dstIp.empty?
+        iocType = 'DestinationIP'
+        iocCacheKeySeen = "IOC_SEEN!!#{iocType}!!#{dataSet}!!#{dstIp}"
+        set_cache_key_value(@redisObj, iocCacheKeySeen, 30, Time.now.to_i)
+    end
+
+    if dnsQuestionDomain && !dnsQuestionDomain.empty?
+        iocType = 'Domain'
+        iocCacheKeySeen = "IOC_SEEN!!#{iocType}!!#{dataSet}!!#{dnsQuestionDomain}"
+        set_cache_key_value(@redisObj, iocCacheKeySeen, 30, Time.now.to_i)
+    end
+
+    if httpDomain && !httpDomain.empty?
+        iocType = 'Domain'
+        iocCacheKeySeen = "IOC_SEEN!!#{iocType}!!#{dataSet}!!#{httpDomain}"
+        set_cache_key_value(@redisObj, iocCacheKeySeen, 30, Time.now.to_i)
+    end
+
+    if fileHashSha256 && !fileHashSha256.empty?
+        iocType = 'FileHashSha256'
+        iocCacheKeySeen = "IOC_SEEN!!#{iocType}!!#{dataSet}!!#{fileHashSha256}"
+        set_cache_key_value(@redisObj, iocCacheKeySeen, 30, Time.now.to_i)
+    end
+
+    if fileHashMD5 && !fileHashMD5.empty?
+        iocType = 'FileHashMD5'
+        iocCacheKeySeen = "IOC_SEEN!!#{iocType}!!#{dataSet}!!#{fileHashMD5}"
+        set_cache_key_value(@redisObj, iocCacheKeySeen, 30, Time.now.to_i)
+    end
 
     event.set('cust_ts_done_aggregated', Time.now.to_i)
     return [event]
